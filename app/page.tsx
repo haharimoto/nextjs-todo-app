e7c9b326007558830fc2c32f58450d9b12aa505b
Hakuyo Harimoto <haharimoto@gmail.com>
2024-01-05 17:43:04 +0900

    added basic style and made a simple todo list on home page using Prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
+import { prisma } from "@/db"
+import Link from "next/link"
 
-export default function Home() {
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany()
+  // await prisma.todo.create({ data: {title: 'test2', complete: false} })
+
   return (
-    <div>
-      NEXTJS APP
-    </div>
+    <>
+      <header className="flex justify-between">
+        <h1 className="text-2xl">TODOs</h1>
+        <Link
+          href="/new"
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        >
+          Jump to New Page
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
   )
 }
