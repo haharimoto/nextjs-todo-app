3ee6b79df4cd1895d1e8b8ff8d2dff8d6ffda107
Hakuyo Harimoto <haharimoto@gmail.com>
2024-01-29 14:45:01 +0900

    Amplify build error likely caused by SSR in Nextjs so temporarily changing the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,46 @@
-import { prisma } from "@/db"
-import Link from "next/link"
-import ToDoItem from "@/components/ToDoItem"
+// import { prisma } from "@/db"
+// import Link from "next/link"
+// import ToDoItem from "@/components/ToDoItem"
 
-// have this function here outside of Home component so that it can be used anywhere
-function getToDos() {
-  return prisma.todo.findMany()
-}
+// // have this function here outside of Home component so that it can be used anywhere
+// function getToDos() {
+//   return prisma.todo.findMany()
+// }
 
-async function toggleToDo(id: string, complete: boolean) {
-  "use server"
-  await prisma.todo.update({ where: { id }, data: { complete } })
-  // console.log(id, complete)
-}
+// async function toggleToDo(id: string, complete: boolean) {
+//   "use server"
+//   await prisma.todo.update({ where: { id }, data: { complete } })
+//   // console.log(id, complete)
+// }
 
-export default async function Home() {
-  const todos = await getToDos()
-  // await prisma.todo.create({ data: {title: 'test3', complete: false} })
+// export default async function Home() {
+//   const todos = await getToDos()
+//   // await prisma.todo.create({ data: {title: 'test3', complete: false} })
 
+//   return (
+//     <>
+//       <header className="flex justify-between">
+//         <h1 className="text-2xl">TODOs</h1>
+//         <Link
+//           href="/new"
+//           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+//         >
+//           Jump to New Page
+//         </Link>
+//       </header>
+//       <ul className="pl-4">
+//         {todos.map(todo => (
+//           <ToDoItem key={todo.id} toggleToDo={toggleToDo} {...todo}/>
+//         ))}
+//       </ul>
+//     </>
+//   )
+// }
+
+export default function Home() {
   return (
     <>
-      <header className="flex justify-between">
-        <h1 className="text-2xl">TODOs</h1>
-        <Link
-          href="/new"
-          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-        >
-          Jump to New Page
-        </Link>
-      </header>
-      <ul className="pl-4">
-        {todos.map(todo => (
-          <ToDoItem key={todo.id} toggleToDo={toggleToDo} {...todo}/>
-        ))}
-      </ul>
+      Next.js
     </>
   )
 }
